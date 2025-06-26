8973c4dd4feef5f91419985341c971ef7681b757
AJ Caldwell <tomatrow@gmail.com>
2025-06-26 10:25:09 -0700

    Updates

---
+++ _app/env.js
@@ -0,0 +1,1 @@
+export const env={"PUBLIC_POCKETBASE_URL":"http://127.0.0.1:8090","PUBLIC_FRONTEND_URL":"http://localhost:5173"}
---
+++ _app/immutable/chunks/KjQxFrNl.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_byzw53)==null?void 0:s.base)??"/runic-pocketbase-collection";var e;const a=((e=globalThis.__sveltekit_byzw53)==null?void 0:e.assets)??t;export{a,t as b};
