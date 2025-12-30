5abe43486a294018b7272fccf6f8e7ef784e1402
AJ Caldwell <tomatrow@gmail.com>
2025-12-29 18:56:45 -0800

    Run format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import devtoolsJson from "vite-plugin-devtools-json"
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [sveltekit(), devtoolsJson()]
-});
+})
